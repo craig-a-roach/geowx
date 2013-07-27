fb1ad19c93e12e1218a2c6bd482b60c74ba31a3f
craig-a-roach <roach@metservice.com>
2013-07-27 20:03:20 +1200

    Mercator Sphere UT Pass

--- ebx/src/test/java/com/metservice/gallium/projection/TestUnit1ProjectionFactory.java
+++ ebx/src/test/java/com/metservice/gallium/projection/TestUnit1ProjectionFactory.java
@@ -80,7 +80,6 @@ public class TestUnit1ProjectionFactory {
 				param("latitude_of_origin", deg('N', 52, 9, 22.178)), param("scaleFactor", 0.9999079),
 				param("falseEasting", 155000.0), param("falseNorthing", 463000.0) };
 		final WktStructure specE = new WktStructure("PROJCS", "Stereo Oblique Ref", gcsE, p, params, Metres);
-		System.out.println(specE.format());
 		final WktStructure specS = new WktStructure("PROJCS", "Stereo Oblique Ref", GCS_Sphere, p, params, Metres);
 		try {
 			final ProjectedCoordinateSystem pcsE = WktCoordinateSystemFactory.newCoordinateSystemProjected(specE.format());
@@ -111,24 +110,33 @@ public class TestUnit1ProjectionFactory {
 		final WktStructure[] params = { param("central_meridian", 51.0), param("standard_parallel_1", 42.0) };
 		final WktStructure specM = new WktStructure("PROJCS", "Mercator Ref", gcs, p, params, Metres);
 		final WktStructure specK = new WktStructure("PROJCS", "Mercator Ref", gcs, p, params, Kilometres);
+		final WktStructure specS = new WktStructure("PROJCS", "Mercator Ref", GCS_Sphere, p, params, Metres);
 		try {
 			final ProjectedCoordinateSystem pcsM = WktCoordinateSystemFactory.newCoordinateSystemProjected(specM.format());
 			System.out.println(pcsM.toWkt().format());
 			final ProjectedCoordinateSystem pcsK = WktCoordinateSystemFactory.newCoordinateSystemProjected(specK.format());
+			final ProjectedCoordinateSystem pcsS = WktCoordinateSystemFactory.newCoordinateSystemProjected(specS.format());
 			final IGalliumProjection pjM = pcsM.newProjection();
 			final IGalliumProjection pjK = pcsK.newProjection();
+			final IGalliumProjection pjS = pcsS.newProjection();
 			final GalliumPointD ptM = pjM.transform(53.0, -53.0);
 			final GalliumPointD ptK = pjK.transform(53.0, -53.0);
+			final GalliumPointD ptS = pjS.transform(53.0, -53.0);
 			Assert.assertEquals("Easting(m)", 165704.29, ptM.x, 1e-2);
 			Assert.assertEquals("Northing(m)", -5171848.07, ptM.y, 1e-2);
 			Assert.assertEquals("Easting(km)", 165.70429, ptK.x, 1e-2);
 			Assert.assertEquals("Northing(km)", -5171.84807, ptK.y, 1e-2);
+			Assert.assertEquals("Easting(m)", 165267.87, ptS.x, 1e-2);
+			Assert.assertEquals("Northing(m)", -5183571.97, ptS.y, 1e-2);
 			final GalliumPointD piM = pjM.inverseDegrees(ptM.x, ptM.y);
 			final GalliumPointD piK = pjK.inverseDegrees(ptK.x, ptK.y);
+			final GalliumPointD piS = pjS.inverseDegrees(ptS.x, ptS.y);
 			Assert.assertEquals("Lon", 53.0, piM.x, 1e-2);
 			Assert.assertEquals("Lat", -53.0, piM.y, 1e-2);
 			Assert.assertEquals("Lon", 53.0, piK.x, 1e-2);
 			Assert.assertEquals("Lat", -53.0, piK.y, 1e-2);
+			Assert.assertEquals("Lon", 53.0, piS.x, 1e-2);
+			Assert.assertEquals("Lat", -53.0, piS.y, 1e-2);
 			Assert.assertTrue("Inside", pjM.isInside(53.0, -53.0));
 			Assert.assertFalse("Outside", pjM.isInside(53.0, -87.0));
 		} catch (final GalliumSyntaxException ex) {
@@ -284,7 +292,7 @@ public class TestUnit1ProjectionFactory {
 	}
 
 	@Test
-	public void t400_transverseMercator() {
+	public void t400_transverseMercator_OSGB() {
 		final String spheroid = "  SPHEROID[\"Airy 1830\",6377563.396,299.3249646,AUTHORITY[\"EPSG\",\"7001\"]]";
 		final String datum = " DATUM[\"OSGB_1936\",\n" + spheroid
 				+ ",TOWGS84[375,-111,431,0,0,0,0],AUTHORITY[\"EPSG\",\"6277\"]]";
