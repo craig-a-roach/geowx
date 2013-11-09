cb436ee0b74b214309f35a8b6b3316d8ba6e641b
craigroach <craigroach@192.168.2.6>
2013-11-09 16:23:29 +1300

    Argon disk cache ut 1

--- ebx/src/test/java/com/metservice/argon/cache/disk/TestUnit1Mru.java
+++ ebx/src/test/java/com/metservice/argon/cache/disk/TestUnit1Mru.java
@@ -6,15 +6,19 @@
 package com.metservice.argon.cache.disk;
 
 import java.io.File;
+import java.util.Date;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.metservice.argon.ArgonPermissionException;
+import com.metservice.argon.ArgonPlatformException;
 import com.metservice.argon.ArgonServiceId;
+import com.metservice.argon.Binary;
 import com.metservice.argon.Ds;
 import com.metservice.argon.TestHelpC;
+import com.metservice.argon.cache.ArgonCacheException;
 import com.metservice.argon.management.IArgonSpaceId;
 
 /**
@@ -29,9 +33,67 @@ public class TestUnit1Mru {
 		final Probe probe = new Probe();
 		try {
 			final ArgonDiskCacheController.Config cfg = ArgonDiskCacheController.newConfig(probe, SID, SPACE);
+			final ArgonDiskCacheController dcc = ArgonDiskCacheController.newInstance(cfg);
 		} catch (final ArgonPermissionException ex) {
 			Assert.fail(ex.getMessage());
+		} catch (final ArgonPlatformException ex) {
+			Assert.fail(ex.getMessage());
 		}
+	}
+
+	private static class Cacheable implements IArgonDiskCacheable {
+
+		private static Binary createBinary(int bc) {
+			if (bc < 0) return null;
+			final byte[] payload = new byte[bc];
+			for (int i = 0; i < bc; i++) {
+				final int ib = 32 + (i % 64);
+				payload[i] = (byte) ib;
+			}
+			return Binary.newFromTransient(payload);
+		}
+
+		@Override
+		public Binary getContent() {
+			return m_oContent;
+		}
+
+		@Override
+		public String getContentValidator() {
+			return m_ozValidator;
+		}
+
+		public Cacheable(int bcPayload, String ozValidator) {
+			m_oContent = createBinary(bcPayload);
+			m_ozValidator = ozValidator;
+		}
+		private final Binary m_oContent;
+		private final String m_ozValidator;
+	}
+
+	private static class MruRequest implements IArgonDiskCacheMruRequest {
+
+		@Override
+		public String getContentValidator(Date now) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public boolean isValid(Date now, String zContentValidator) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public String qccResourceId() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public MruRequest() {
+		}
+
 	}
 
 	private static class Probe implements IArgonDiskCacheProbe {
@@ -94,5 +156,16 @@ public class TestUnit1Mru {
 		}
 
 		private final String m_qId;
+	}
+
+	private static class Supplier implements IArgonDiskCacheSupplier<MruRequest> {
+
+		@Override
+		public IArgonDiskCacheable getCacheable(MruRequest request)
+				throws ArgonCacheException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
 	}
 }
